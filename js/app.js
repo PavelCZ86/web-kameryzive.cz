9f1a397320ac7925d34c7a41f63a28c13dad493a
Pavel Šnobl <98052560+PavelCZ86@users.noreply.github.com>
2024-09-24 16:22:26 +0200

    Delete js/app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".navigation");
-const btnBurger = document.querySelector(".burger");
-
-btnBurger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-  menu.classList.toggle("active");
-}
